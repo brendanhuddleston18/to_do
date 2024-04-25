4fd480427f60839dbfc4fd163c2bc4ac3b195452
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-04-25 11:44:24 -0500

    adds input text field, list renders on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:to_do/screens/home.dart';
 
 // class Colors {
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,24 +1,81 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: Color.fromARGB(255, 79, 152, 189),
-        leading: const Text("Panel"),
-        middle: const Text("Brendan's To Do List")
-      ),
-      child: Row(
-        children: [
-          SliverList(delegate: delegate),
+  State<Home> createState() => _HomeWidgetState();
+}
 
-          ],
-        ),
+class _HomeWidgetState extends State<Home> {
+  List<String> tasks = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+          backgroundColor: Color.fromARGB(255, 79, 152, 189),
+          leading: Text("Panel"),
+          middle: Text("Brendan's To Do List")),
+      child: Stack(
+        children: [
+          CupertinoListSection(
+              header: const Text('My Reminders'),
+              children: <CupertinoListTile>[
+                for (String task in tasks)
+                  CupertinoListTile(
+                    title: Text(task),
+                    leading: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: CupertinoColors.activeGreen,
+                    ),
+                  ),
+              ]),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 20,
+                ),
+                child: TextInputWidget(
+                  onAddTask: (String newTask) {
+                    setState(() {
+                      (tasks.add(newTask));
+                    });
+                  },
+                ),
+              ))
+        ],
+      ),
     );
+  }
+}
+
+class TextInputWidget extends StatefulWidget {
+  const TextInputWidget({super.key, required this.onAddTask});
+
+  final Function(String) onAddTask;
+
+  @override
+  State<TextInputWidget> createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTextField(
+        controller: controller,
+        placeholder: "Enter a task!",
+        prefix: IconButton(
+          onPressed: () => widget.onAddTask(controller.text),
+          icon: const Icon(CupertinoIcons.add),
+        ));
   }
 }
